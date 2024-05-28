bebb6ec358d847682b9bf017197eda7cb920442f
Marquestt <thatyanecmarques98@live.com>
2024-05-28 17:30:59 -0300

    Formulario atualizado com os campos que faltavam e botão adicionado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,32 @@
 import './Formulario.css';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
+    const equipes = [
+        'Overwatch',
+        'League of Legends',
+        'Fortnite',
+        'Rocket League'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Form foi submetido!');
+    }
+
     return (
         <section className="formulario">
-            <form>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do membro da equipe</h2>
+                <CampoTexto obrigatorio={true} label="Nickname" placeholder="Digite o seu nickname"/>
+                <CampoTexto obrigatorio={true} label="Função" placeholder="Digite a função"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Equipe" itens={equipes}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
